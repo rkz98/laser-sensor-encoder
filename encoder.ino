d5d738c62b7d8e9b99028a4308ac229ffff1f151
Wallace <wbescrovaine@gmail.com>
2020-08-30 22:07:24 -0300

    add

--- encoder.ino
+++ encoder.ino
@@ -6,35 +6,46 @@ unsigned int lastLaserSensorState = 0;
 
 const unsigned int pinLaserSensor = 8;
 
-void setup() {
+void setup()
+{
 	Serial.begin(9600);
 	pinMode(pinLaserSensor, INPUT);
 }
 
-void checkIfCompleteRotation() {
-	if (rotationStep == COMPLETE_ROTATION) {
+void checkIfCompleteRotation()
+{
+	if (rotationStep == COMPLETE_ROTATION)
+	{
 		rotationStep = 0;
 		completeRotations += 1;
 	}
 }
 
-void startRunning() {
+void startRunning()
+{
+	unsigned int lastLaserSensorState = 0;
 	unsigned int laserSensorState = digitalRead(pinLaserSensor);
 
-	if (laserSensorState == LOW) {
-		if (laserSensorState == HIGH) {
+	if (laserSensorState == LOW)
+	{
+		if (laserSensorState == HIGH)
+		{
 			rotationStep += 1;
 			lastLaserSensorState = LOW;
 			checkIfCompleteRotation();
 		}
-	} else if (lastLaserSensorState == LOW) {
+	}
+	else if (lastLaserSensorState == LOW)
+	{
 		rotationStep += 1;
 		lastLaserSensorState = HIGH;
 		checkIfCompleteRotation();
 	}
 }
 
-void loop() {
+void loop()
+{
+	Serial.println(completeRotations);
 	startRunning();
 	Serial.println(completeRotations);
 }
