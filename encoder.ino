52acbc234ff7e03b70d179224a27b6195aef370f
Kauê Rodrigues <kauerodrigues98@gmail.com>
2020-08-31 20:14:36 -0300

    ALEGRIA!

--- encoder.ino
+++ encoder.ino
@@ -6,46 +6,35 @@ unsigned int lastLaserSensorState = 0;
 
 const unsigned int pinLaserSensor = 8;
 
-void setup()
-{
+void setup() {
 	Serial.begin(9600);
 	pinMode(pinLaserSensor, INPUT);
 }
 
-void checkIfCompleteRotation()
-{
-	if (rotationStep == COMPLETE_ROTATION)
-	{
+void checkIfCompleteRotation() {
+	if (rotationStep == COMPLETE_ROTATION) {
 		rotationStep = 0;
 		completeRotations += 1;
 	}
 }
 
-void startRunning()
-{
-	unsigned int lastLaserSensorState = 0;
+void startRunning() {
 	unsigned int laserSensorState = digitalRead(pinLaserSensor);
 
-	if (laserSensorState == LOW)
-	{
-		if (laserSensorState == HIGH)
-		{
+	if (laserSensorState == LOW) {
+		if (lastLaserSensorState == HIGH) {
 			rotationStep += 1;
 			lastLaserSensorState = LOW;
 			checkIfCompleteRotation();
 		}
-	}
-	else if (lastLaserSensorState == LOW)
-	{
+	} else if (lastLaserSensorState == LOW) {
 		rotationStep += 1;
 		lastLaserSensorState = HIGH;
 		checkIfCompleteRotation();
 	}
 }
 
-void loop()
-{
-	Serial.println(completeRotations);
+void loop() {
 	startRunning();
 	Serial.println(completeRotations);
 }
